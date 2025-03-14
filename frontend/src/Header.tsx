134c60f05bc4192da13fb7aa6b87400486464de4
parkerfellows <fellowsparker@gmail.com>
2025-03-14 15:50:53 -0600

    finsihed header

--- frontend/src/Header.tsx
+++ frontend/src/Header.tsx
@@ -3,6 +3,7 @@ function Header() {
   return (
     <header>
       <h1>Bowling League Database!!🎳</h1>
+      <p>This page will display all the data for the bowlers in our league who belong to the Marlins or the Sharks.</p>
     </header>
   );
 }
